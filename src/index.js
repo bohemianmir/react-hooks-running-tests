42af952d56bbf964dc62e36b73f60b3e0a8e8451
miraamer <mira.amr13@gmail.com>
2023-06-01 22:50:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import { render } from "@testing-library/react";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+  
